52a00299eb12fc8ae967b1d555754f5516ecb27d
cedrus172 <64965926+cedrus172@users.noreply.github.com>
2021-09-20 21:32:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,18 +6,42 @@ function checkKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '38') {
-        // up arrow
-        document.getElementById("tank").style["top"] = parseInt(tank.style["top"], 10 ) - 19 + "px";
+        moveUp();
     }
     else if (e.keyCode == '40') {
-        document.getElementById("tank").style["top"] = parseInt(tank.style["top"], 10 ) + 19 + "px";
+        moveDown();
     }
     else if (e.keyCode == '37') {
-        document.getElementById("tank").style["left"] = parseInt(tank.style["left"], 10 ) - 19 + "px";
+        moveLeft();
     }
     else if (e.keyCode == '39') {
-        document.getElementById("tank").style["left"] =  parseInt(tank.style["left"], 10 ) + 19 + "px";
-    }
+        moveRight();
+    }   
+}
+
+function moveUp()
+{
+    document.getElementById("tank").style["top"] = parseInt(tank.style["top"], 10 ) - 19 + "px";
+    checkPosition();
+}
+function moveDown()
+{
+    document.getElementById("tank").style["top"] = parseInt(tank.style["top"], 10 ) + 19 + "px";
+    checkPosition();
+}
+function moveLeft()
+{
+    document.getElementById("tank").style["left"] = parseInt(tank.style["left"], 10 ) - 19 + "px";
+    checkPosition();
+}
+function moveRight()
+{
+    document.getElementById("tank").style["left"] =  parseInt(tank.style["left"], 10 ) + 19 + "px";
+    checkPosition();
+}
+
+function checkPosition()
+{
     if(parseInt(document.getElementById("tank").style["top"], 10) < 0 || parseInt(document.getElementById("tank").style["top"], 10) > 171)
     {
         alert("Bạn đã thua cuộc");
